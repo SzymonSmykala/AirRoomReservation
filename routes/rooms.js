ce2175d6ec3396f6a3088242b697e7e617b8d1e2
Szymon Smykała <ctk7000@gmail.com>
2020-03-22 11:13:22 +0100

    Add orm (#7)
    * initial work
    
    * added sample test
    
    * added connection.js with sample db connection
    
    * added room and user entity
    
    * added readme
    
    * updated README.md
    
    * Added rest for rooms
    
    * cleanedup
    
    * restored package log

---
+++ routes/rooms.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const Room = require('../models/Room');
+
+
+router.get('/:roomId', async (req, res) => {
+    try {
+        const room = await Room.findById(req.params.roomId);
+        res.json(room);
+    }catch (e) {
+        res.json(e);
+    }
+});
+
+router.get('/', async (req, res) =>{
+    try{
+        const rooms = await Room.find();
+        res.json(rooms);
+    }catch (error) {
+        res.json(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+   const room = new Room({
+        name: req.body.name,
+        id: req.body.id
+   });
+    try {
+        const savedRoom = await room.save();
+        res.json(savedRoom);
+    }catch (error) {
+        res.json(error);
+    }
+
+});
+
+
+router.delete('/roomId', async (req, res) => {
+    try{
+        const rooms = await Room.removeById(req.params.roomId);
+        res.json(rooms);
+    }catch (error) {
+        res.json(error);
+    }
+
+});
+
+router.patch('/', async (req, res) => {
+
+    try{
+        const room = new Room({
+            name: req.body.name,
+            id: req.body.id,
+            _id: req.body._id
+        });
+        const updatedRoom = await Room.updateOne(room);
+
+       res.json(updatedRoom);
+    }catch (error) {
+        res.json(error);
+    }
+});
+
+module.exports = router;
