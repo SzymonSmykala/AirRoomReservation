856d80a0cf2a9b5ca2204b9c51d639ffc0101fc9
SzymonSmykala <ctk7000@gmail.com>
2020-04-05 14:10:39 +0200

    added reactstrap

--- client/src/api/RoomService.js
+++ client/src/api/RoomService.js
@@ -1,12 +1,15 @@
 import API_ENDPOINT from "../Constants";
 
 export class Room {
-
+    _id;
+    __v;
+    name;
+    costPerDay;
 }
 
 export class RoomService {
 
-    async fetchRoomsAsync(){
+    async fetchRoomsAsync() : Array<Room>{
         let books = await fetch(API_ENDPOINT + "/rooms");
         let booksObjects = JSON.parse(await books.text());
         console.log(booksObjects);
