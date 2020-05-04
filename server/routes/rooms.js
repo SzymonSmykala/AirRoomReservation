acfc8dabfafa70769adec01081ac11e68c489a91
SzymonSmykala <ctk7000@gmail.com>
2020-05-04 21:15:08 +0200

    basic reservation getting by dates

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Room = require('../models/Room');
+const Reservation = require('../models/Reservation');
 
 
 router.get('/:roomId', async(req, res) => {
@@ -55,6 +56,24 @@ router.patch('/', async(req, res) => {
   } catch (error) {
     res.json(error);
   }
+});
+
+router.post('/roomsForDates', async(req, res) => {
+  let roomId = req.body.roomId;
+  let startDate = req.body.startDate;
+  let endDate = req.body.endDate;
+
+  let resultArray = [];
+  const rooms = await Room.find();
+
+  let startDateDate = new Date(startDate);
+  let endDateDate = new Date(endDate);
+
+  for (const room of rooms) {
+      const reservations = await Reservation.find({"startDate": {"$gte": startDateDate}, "endDate": {"$lt": endDateDate}}).where('room').eq(roomId);
+      resultArray.push(reservations)
+  }
+  res.json(resultArray);
 });
 
 module.exports = router;
